4df3ded97bd06abbc132f2fbc648a8f8097472f5
YeganeGholipour <yeganegholiour@gmail.com>
2025-07-08 18:16:09 +0330

    refactor vga display

--- include/serial.h
+++ include/serial.h
@@ -1,8 +1,9 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-void serial_init();
 void serial_writechar(char c);
 void serial_writestring(const char* str);
+void serial_writeint(int num);
+void serial_writehex(uint32_t num);
 
 #endif
