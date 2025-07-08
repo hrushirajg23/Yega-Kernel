55396d93113ae5cad0c7fe726cd53c6e45603d50
YeganeGholipour <yeganegholiour@gmail.com>
2025-07-08 18:16:23 +0330

    add eoi function

--- include/pic.h
+++ include/pic.h
@@ -1,6 +1,8 @@
 #ifndef PIC_H
 #define PIC_H
 
+#define PIC_EOI 0x20
+
 #define PIC1 0x20 /* base address for master PIC */
 #define PIC2 0xA0 /* base address for slave PIC */
 #define PIC1_COMMAND PIC1
@@ -11,9 +13,9 @@
 void remap_pic(int offset1, int offset2);
 void IRQ_set_mask(uint8_t IRQline);
 void IRQ_clear_mask(uint8_t IRQline);
+void send_EOI(uint8_t irq);
 
 uint16_t pic_get_irr(void);
 uint16_t pic_get_isr(void);
-
 
 #endif
