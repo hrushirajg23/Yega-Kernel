f52405737761f553daa00bf6c4d2384dcad969db
hrushiraj <hrushirajg23@gmail.com>
2025-12-05 02:17:16 +0530

    feat(mm): add compound page support functions
    - Implement compound_head() to handle multi-page allocations
    - Add virt_to_head_page() to get the head page from virtual address
    
    Compound pages are used by SLAB allocator when allocating multiple
    contiguous pages for large slabs.

--- include/mm.h
+++ include/mm.h
@@ -2,6 +2,7 @@
 #define _MM_H
 
 #include "zone.h"
+#include "page.h"
 
 #define ALIGN_PAGE(address) (address - (address % PAGE_SIZE))
 #define IS_PAGE_ALIGNED(address) (((address) % PAGE_SIZE) == 0)
@@ -15,4 +16,17 @@ void memset(void *addr, char val, unsigned int size);
 void *memcpy(void *dest, void *src, unsigned int size);
 void init_mem(multiboot_info_t *);
 
+
+static inline struct page *compound_head(struct page *page)
+{
+	// if (unlikely(PageTail(page)))
+		return page->first_page;
+	// return page;
+}
+
+static inline struct page *virt_to_head_page(const void *x)
+{
+	struct page *page = virt_to_page(x);
+	return compound_head(page);
+}
 #endif
