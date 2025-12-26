c280be946c1615be20a75fee43dc1ee3a185e517
hrushiraj <hrushirajg23@gmail.com>
2025-12-27 00:41:15 +0530

    feat(i/o): input output macros and functions

---
+++ include/hardware.h
@@ -0,0 +1,44 @@
+#ifndef _HARDWARE_H
+#define _HARDWARE_H
+
+#define BOOTLOADER_OFFSET   0x2000
+#define KERNEL_OFFSET       0xc0000000
+
+/* note: we must use e.g. "unsigned short" instead of "uint16_t" 
+ * so that the underlying "asm" command is happy with its operand constraints. */
+
+unsigned char port_byte_in(unsigned short port);
+void port_byte_out(unsigned short port, unsigned char data);
+unsigned short port_word_in(unsigned short port);
+void port_word_out(unsigned short port, unsigned short data);
+void port_multiword_out(unsigned short port, unsigned char *data, unsigned long size);
+void port_multiword_in(unsigned short port, unsigned char *data, unsigned long size);
+
+void port_dword_out(unsigned short port, unsigned int data);
+unsigned int port_dword_in(unsigned short port);
+
+
+void sys_exit();
+
+/* debugging */
+#define HALT()  while (1) { }
+
+// #define PRINT_EIP()  do { \
+//     uint32_t eip; \
+//     print("%EIP: ");\
+//     asm volatile ("call _test\n\t" \
+//             "_test: pop %%eax\n\t"\
+//             "mov %%eax, %0;": "=a" (eip)); \
+//     print_word(eip); \
+//     HALT();\
+//     } while (0);
+
+// #define DUMP_STACK() do { \
+//     uint32_t test;\
+//     asm volatile ("mov %%esp, %0" : "=r" (test) :);\
+//     print("esp: "); print_word(test);\
+//     asm volatile ("mov %%ebp, %0" : "=r" (test) :);\
+//     print("ebp: "); print_word(test);\
+//     } while (0);
+
+#endif
---
+++ kernel/system.c
@@ -0,0 +1,47 @@
+/* hardware.c is mostly from ToaruOS */
+#include "hardware.h"
+
+unsigned char port_byte_in(unsigned short port) {
+    unsigned char result;
+    asm volatile ("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void port_byte_out(unsigned short port, unsigned char data) {
+    asm volatile ("out %%al, %%dx" : :"a" (data), "d" (port));
+}
+
+unsigned short port_word_in(unsigned short port) {
+    unsigned short result;
+    asm volatile ("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    return result;
+}
+
+void port_word_out(unsigned short port, unsigned short data) {
+    asm volatile ("out %%ax, %%dx" : :"a" (data), "d" (port));
+}
+
+/* TODO: why can't you just loop port_word_out? that doesn't work, but why? */
+void port_multiword_out(unsigned short port, unsigned char *data, unsigned long size) {
+    asm volatile ("rep outsw" : "+S" (data), "+c" (size) : "d" (port));
+}
+
+void port_multiword_in(unsigned short port, unsigned char *data, unsigned long size) {
+    // what does "memory" do?
+    // -> https://stackoverflow.com/questions/14950614/working-of-asm-volatile-memory
+    asm volatile ("rep insw" : "+D" (data), "+c" (size) : "d" (port) : "memory");
+}
+
+void port_dword_out(unsigned short port, unsigned int data) {
+    asm volatile ("outl %%eax, %%dx" : : "dN" (port), "a" (data));
+}
+
+unsigned int port_dword_in(unsigned short port) {
+    unsigned int rv;
+    asm volatile ("inl %%dx, %%eax" : "=a" (rv) : "dN" (port));
+    return rv;
+}
+
+void sys_exit() {
+    asm volatile ("end: \n\t jmp end");
+}
