e4560a43f7092895156ddbad70fd6f33287bf88c
hrushiraj <hrushirajg23@gmail.com>
2026-01-21 09:48:31 +0530

    fix(buffer): getblk scenario 2 error - buffer didn't update its block number after getting put in new hashqueue - scenario 3 handling currently with synchronous disk write, will change it async after disk driver gets updated with interrupt I/O

---
+++ include/buffer.h
@@ -0,0 +1,43 @@
+#ifndef _BUFFER_H
+#define _BUFFER_H
+
+#include "ufs.h"
+
+#define hash_fn(bno, devno) (bno % devno)
+
+
+/* BUFFER_CACHE
+ * ======================================================================================
+ */
+
+#define BH_lock 1 << 0
+#define BH_dirty 1 << 1
+#define BH_uptodate 1 << 2
+#define BH_delay 1 << 3
+#define BH_old 1 << 5
+
+
+struct buffer_head{
+    unsigned short flags;
+    unsigned int b_count; //buffer ref count
+    char* b_data; //ptr to data //1024 bytes
+    unsigned short b_dev; //if ==0 means free
+    unsigned long b_blocknr; //block number
+    struct list_head b_hash;
+    struct list_head b_free;
+};
+
+struct buffer_cache {
+    struct list_head b_hash[DEV_NO]; //the buffer hashmap
+    struct list_head b_free; //the freelist
+};
+
+
+/* buffer cache operations */
+void create_buffer_cache(void);
+struct buffer_head *bread(unsigned short dev_no, unsigned long blocknr);
+void bwrite(struct buffer_head *bh);
+void brelse(struct buffer_head *bh);
+struct buffer_head *getblk(unsigned short dev_no, unsigned long blocknr);
+
+#endif
